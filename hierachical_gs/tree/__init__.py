1b2a4b3adc726f52e2ba8322eb659fa4ce32db54
saliteta <butianxi@usc.edu>
2025-10-22 18:50:59 -0700

    Remove old files after restructuring
    - Remove old root-level files that are now in scene_decompose/
    - Clean up repository structure
    - All package files now properly organized in scene_decompose/ subdirectory

--- hierachical_gs/tree/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Tree data structures for hierarchical processing.
-"""
-
-from .tree import HierarchicalTree, TreeNode, LayerInfo
-from .pca_tree import PCABinaryTree
-
-__all__ = ['HierarchicalTree', 'TreeNode', 'LayerInfo', 'PCABinaryTree']
